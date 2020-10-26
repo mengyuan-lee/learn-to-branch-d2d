81bc9d465fa39989bbaa435257e2001c3091df4e
mengyuan-lee <63926987+mengyuan-lee@users.noreply.github.com>
2020-10-25 23:09:16 -0400

    Update Fun_chGain.m

--- Fun_chGain.m
+++ Fun_chGain.m
@@ -1,5 +1,5 @@
 function H = Fun_chGain(ChannelNum,CUE_x,CUE_y)
-%input: ChannelNum : number of channel
+% input: ChannelNum : number of channel
 %       CUE_x: abcissa of users
 %       CUE_y; ordinate of users
 % output:H: Channel Gain
@@ -11,7 +11,7 @@ for  i = 1:ChannelNum
             Dis(i)= sqrt(CUE_x(i)^2 + CUE_y(i)^2);
 end
 
-%the channel gain of user i
+% the channel gain of user i
 H = zeros(ChannelNum,1);
 for k = 1:ChannelNum
         H(k) = channel_fading_UE_to_BS(Dis(k)/1000);
