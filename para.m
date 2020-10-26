3aa26c7cdecfb833769c148f8d05684c08da3494
mengyuan-lee <63926987+mengyuan-lee@users.noreply.github.com>
2020-10-25 23:14:10 -0400

    Update para.m

--- para.m
+++ para.m
@@ -1,17 +1,17 @@
 function [a,b,p_max] = para(K,L,R_min_C,P_max_D,P_max_C,h_CD,h_D,h_CB,h_DB)
-%output:
-%        a/b:K*L matrix
-%        P_max_D: maximum d2d linnk power
-%        p_max: K*L matrix
-%input:  K number of CUs
-%        L number of D2D links
-%        R_min_C: minimum rate of CUs
-%        P_max_D: maximum d2d linnk power
-%        P_max_C: maximum CUs
-%        h_CD:channel gain between CU and the receiver of D2D pair 
-%        h_D:channel gain of D2D pair 
-%        h_CB:channel gain between CU and base station
-%        h_CB:channel gain between the transmitter of D2D pair and base station
+% output:
+%         a/b:K*L matrix
+%         P_max_D: maximum d2d linnk power
+%         p_max: K*L matrix
+% input:  K number of CUs
+%         L number of D2D links
+%         R_min_C: minimum rate of CUs
+%         P_max_D: maximum d2d linnk power
+%         P_max_C: maximum CUs
+%         h_CD:channel gain between CU and the receiver of D2D pair 
+%         h_D:channel gain of D2D pair 
+%         h_CB:channel gain between CU and base station
+%         h_CB:channel gain between the transmitter of D2D pair and base station
 
 N0PSD = -174; % noise power spectrum density, dBm/Hz
 N0 = 10.^((N0PSD-30)/10)*1*10^6;
